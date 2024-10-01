c2b104e758602b119f2678602ff329a9d70262aa
shafikul7 <shafikul1170@gmail.com>
2024-10-01 13:44:11 +0600

    project setup to compleate project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+import React_Assignment from './Components/React_Assignment'
+
+function App() {
+ 
+
+  return (
+    <>
+    <React_Assignment></React_Assignment>
+    </>
+  )
+}
+
+export default App
